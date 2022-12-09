d309a4c0d90721fc215ec0bd09204227605d734b
ThomasDarrieumerlou <thomas.darrieumerlou@epitech.eu>
2022-12-09 15:10:57 +0100

    feat(authorizer) : add authorizer in api-gateway module

--- main.tf
+++ main.tf
@@ -68,6 +68,7 @@ module "api_gateway" {
             lambda_arn             = format("%s%s", var.base_lambda_arn, "graphql")
             payload_format_version = "2.0"
             timeout_milliseconds   = 12000
+            authorizer_key = "cognito"
         }
 
         "POST /graphql" = {
@@ -80,6 +81,7 @@ module "api_gateway" {
             lambda_arn = format("%s%s", var.base_lambda_arn, "users")
             payload_format_version = "2.0"
             timeout_milliseconds = 12000
+            authorizer_key = "cognito"
         }
 
         "PUT /users" = {
@@ -111,18 +113,33 @@ module "api_gateway" {
         # }
     }
 
-    # authorizers = {
-    #   "azure" = {
-    #     authorizer_type  = "JWT"
-    #     identity_sources = "$request.header.Authorization"
-    #     name             = "azure-auth"
-    #     audience         = ["d6a38afd-45d6-4874-d1aa-3c5c558aqcc2"]
-    #     issuer           = "https://sts.windows.net/aaee026e-8f37-410e-8869-72d9154873e4/"
-    #   }
-    # }
+    authorizers= {
+        "cognito" = {
+            authorizer_type  = "JWT"
+            identity_sources = "$request.header.Authorization"
+            name             = "cognito-auth"
+            audience = [aws_cognito_user_pool_client.client.id]
+            issuer   = "https://${aws_cognito_user_pool.pool.endpoint}"
+        }
+    }
 
     default_route_settings= {
         throttling_burst_limit   = 200
         throttling_rate_limit    = 100
     }
+
+}
+    // authorizer 
+resource "aws_cognito_user_pool" "pool" {
+    name = "api-gateway-pool"
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+    name = "api-gateway"
+    user_pool_id = aws_cognito_user_pool.pool.id
+    explicit_auth_flows = [
+        "ALLOW_USER_PASSWORD_AUTH",
+        "ALLOW_USER_SRP_AUTH",
+        "ALLOW_REFRESH_TOKEN_AUTH"
+    ]
 }
